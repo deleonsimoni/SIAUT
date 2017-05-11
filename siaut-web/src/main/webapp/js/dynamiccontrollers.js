cb38e4e9f47fd480c0b3927b041189ce70ba0839
cilde <cilde@DESKTOP-72B0MO9.home>
2017-05-10 22:37:00 -0300

    Commit referente ao merge dynamic e principal

--- siaut-web/src/main/webapp/js/dynamiccontrollers.js
+++ siaut-web/src/main/webapp/js/dynamiccontrollers.js
@@ -1,39 +1,13 @@
-	/*
+/*
 	Aqui são definidos todos os controllers a serem utilizados pelos estados de sua aplicação.
 	Cada estado � composto por um controller (js) e uma tela (html)
 	Os estados são definidos no App.js
 	As páginas são armazenadas na pasta pages
 */
 var DEFAULT_VIEW = "splash";
-angular.module('webApp').controller('SplashController', function ($scope, $rootScope, Log, WebServiceX, Analytics, Error, Utils) {
-        Log.debug("SplashController()");
-    });
 
 angular.module('webApp').controller('PrincipalController', function ($scope, $rootScope, Log, WebServiceX, Analytics, Error, Utils, Alert) {
         Log.debug("PrincipalController()");
-        
-        //IMPLEMENTAR OS BOTOES        
-        $rootScope.currentview.id = 'principal';
-        $rootScope.currentview.group = 'Principal';
-        $rootScope.currentview.title = 'Principal';
-        $rootScope.currentview.icon = 'fa-home';
-        $rootScope.currentview.locked = true;
-		$rootScope.currentview.menu = true;
-        $rootScope.currentview.description = 'Tela Principal';       
-        $scope.lampadaCozinhaLigado = false;
-        $scope.lampadaLigaDesligada = 'aaaaaaaaaaaaaaaaa';
-        $scope.items = [];
-        $scope.id = '11';
-        $scope.status = '12';
-   //     $scope.addItem = function (itemCodigo, itemDescricao, itemPreco, itemQuantidade) {
-     //       $scope.items.push({
-       //         codigo: itemCodigo,
-         //       descricao: itemDescricao,
-           //     preco: itemPreco,
-             //   quantidade, itemQuantidade
-      //      });
-        //};
-
         
         $scope.ligarBotao = function(componentes) {
         	WebServiceX.read("ws/automacao/ligar/"+componentes.lngId+"/"+componentes.lngSituacao, $rootScope.headers)
@@ -103,15 +77,104 @@ angular.module('webApp').controller('PrincipalController', function ($scope, $ro
         }; 
         
         $scope.carregarConfiguracoes();
-        
-        $scope.abrir_produto = function(codigo){
-            console.log('produto aberto : ' + codigo);
-        }; 
-        
-        $scope.abrir_produto = function(codigo){
-            console.log('produto aberto : ' + codigo);
-        };
-        
-        
     });
+
+angular.module('webApp').controller('LoginController', function ($scope, $rootScope, Log, WebServiceX, Analytics, Error, Utils, Alert) {
+    Log.debug("LoginController()");
+    $scope.user = {};
+    $scope.userCadastro = {};
+    $scope.isCadastrar=true;
+    
+    $scope.queroLogar = function(vontade) {
+    	
+    	$scope.isCadastrar = vontade;
+    	
+    }
+    
+    $scope.logar = function() {
+    	if($scope.user.login && $scope.user.pwd){
+    		
+    		WebServiceX.create("ws/usuario/logar", JSON.stringify($scope.user))
+        	.then(function(res) {
+          		if(!res.temErro) {
+          			$rootScope.headers = {token: res.token};
+          			$rootScope.usuario = {nome: res.usuario};
+          			$rootScope.go("principal");
+        			$scope.$apply();
+        		} else if(res.temErro) {
+        			//Erro esperado
+        			Alert.showMessage("Canal fechado", res.msgsErro[0]);
+        			console.info(res.msgsErro[0]);
+        			$scope.$apply();
+        		}
+        	}, function(xhr, status, err) {
+        		
+        		//Erro Inesperado
+      				var message = "Falha ao executar ação";
+      				if (xhr && xhr.responseText) {
+        				try {
+        					var response = JSON.parse(xhr.responseText);
+          					if (response && response.msgsErro && response.msgsErro.length > 0) {
+          						message = response.msgsErro[0];
+          					}	        					        					
+        				} catch(ignore) {
+        				}
+      				}
+      				Error.handler(message, err);
+            		if (err == UNAUTH) {
+            				$rootScope.goAuth();
+            		}
+        	});
+    	} else {
+  			Alert.showMessage("Atenção", "Preencha os campos corretamente!");
+
+    	}
+    	
+    };   
+    
+    $scope.cadastrar = function() {
+    	if($scope.userCadastro.email
+    			&& $scope.userCadastro.senha
+    			&& $scope.userCadastro.documento
+    			&& $scope.userCadastro.telefone){
+    		
+    		$scope.userCadastro.senha = sha1($scope.userCadastro.senha);
+    		
+	    	WebServiceX.create("ws/usuario/cadastrar", JSON.stringify($scope.userCadastro))
+	    	.then(function(res) {
+	      		if(!res.temErro) {
+	      			Alert.showMessage("Bem vindo!", "Seu usuario foi cadastrado com sucesso!");
+	      			delete $scope.userCadastro;
+	      			$scope.isCadastrar=true;
+	    			$scope.$apply();
+	    		} else if(res.temErro) {
+	    			//Erro esperado
+	    			Alert.showMessage("Canal fechado", res.msgsErro[0]);
+	    			console.info(res.msgsErro[0]);
+	    			$scope.$apply();
+	    		}
+	    	}, function(xhr, status, err) {
+	    		
+	    		//Erro Inesperado
+	  				var message = "Falha ao executar ação";
+	  				if (xhr && xhr.responseText) {
+	    				try {
+	    					var response = JSON.parse(xhr.responseText);
+	      					if (response && response.msgsErro && response.msgsErro.length > 0) {
+	      						message = response.msgsErro[0];
+	      					}	        					        					
+	    				} catch(ignore) {
+	    				}
+	  				}
+	  				Error.handler(message, err);
+	        		if (err == UNAUTH) {
+	        				$rootScope.goAuth();
+	        		}
+	    	});
+    	} else {
+  			Alert.showMessage("Atenção", "Preencha os campos corretamente!");
+    	}
+    };   
+    
+});
 
