1f353836fdea412da79c6fe4694794b24325017f
DeleonxD <deleonsimoni@gmail.com>
2019-06-11 02:00:15 -0300

    inicio projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ContainerComponent } from './container/container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'login'
+  },
+  {
+    path: 'container', component: ContainerComponent, children:[
+      {
+      path: 'dashboard', component: DashboardComponent
+      },
+      {
+        path: 'container', pathMatch: 'full', redirectTo: 'dashboard'
+      }
+   ]
+
+  },
+  {
+    path: 'login', component: LoginComponent  
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule] 
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SplashComponent } from './splash/splash.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContainerComponent } from './container/container.component';
+import { ContainerModule } from './container/container.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    SplashComponent,
+    PrincipalComponent,
+    MenuComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
+    ContainerModule,
     AppRoutingModule
   ],
   providers: [],
