5328d50af70ec3fcc583cbbd21af63d4dec52e5a
DeleonxD <deleonsimoni@gmail.com>
2019-06-14 22:36:27 -0300

    arquitetura mean

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+
+const routes: Routes = [
+  
+  {
+  path: '', pathMatch: 'full', redirectTo: '/auth/login'
+  },
+
+  {
+  path: 'auth',
+  children: [{
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full'
+  }, {
+    path: 'login',
+    component: LoginComponent
+  }, {
+    path: 'register',
+    component: RegisterComponent
+  }]
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class AuthRoutingModule { }
